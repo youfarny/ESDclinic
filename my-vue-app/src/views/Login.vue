9d7e9314179732f0ab6d5d9a76513b74cbfbbe4f
youfarny <faith.ong.2023@scis.smu.edu.sg>
2025-04-10 23:33:37 +0800

    name change

--- my-vue-app/src/views/Login.vue
+++ my-vue-app/src/views/Login.vue
@@ -10,7 +10,7 @@
           </div>
         </div>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Healthcare Portal
+          SMUDOC
         </h2>
         <p class="mt-2 text-center text-sm text-gray-600">
           Sign in to access your account
